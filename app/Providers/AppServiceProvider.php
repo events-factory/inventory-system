3876bf447460925e4d8ab0d8a96bc05a9ee84e92
Ntege Oscar <65177621+ntegeoscar@users.noreply.github.com>
2025-07-24 11:18:55 +0200

    Removed changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('local')) {
+            URL::forceScheme('https');
+        }
         //
         View::addNamespace('internalPages', base_path('app/Filament/Resources/ApprovedEventResource/Pages'));
         
