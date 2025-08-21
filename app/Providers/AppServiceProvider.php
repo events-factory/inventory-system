4b547c58fd07b81af78945d2405434424f1b59f8
Ntege Oscar <65177621+ntegeoscar@users.noreply.github.com>
2025-08-21 17:07:30 +0200

    Email and Item filter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,9 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('local')) {
-            URL::forceScheme('https');
-        }
         //
         View::addNamespace('internalPages', base_path('app/Filament/Resources/ApprovedEventResource/Pages'));
         
